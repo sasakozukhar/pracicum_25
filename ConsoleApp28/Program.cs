48971e0480fac4418f3644aeb8df8dfb03b63d13
sasakozukhar <sasakozuhar93@gmail.com>
2025-02-16 19:21:27 +0200

    Add files via upload

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("n=");
+            //int n=int .Parse(Console.ReadLine());
+            //int[]mas= new int[n];
+            //for(int i= 0; i<n; i++)
+            //{
+            //    Console.Write($"mas[{i}]=");
+            //    mas[i]=int.Parse(Console.ReadLine());
+            //}
+            //for (int i=0; i<n; i++)
+            //{
+            //    Console.WriteLine($"mas[{i}]={mas[i]}");
+            //}
+
+            //int[] omas ={ 3, -3, 5, 6, -7, 8};
+            //int[] omas= new int []{3, -3, 5, 6, -7, 8};
+            //foreach (var element  in omas)
+            //{
+            //    Console.Write($"{element}\t");
+
+            //}
+
+            //int[] omas=new int[6];
+            //Random rnd = new Random();
+            //for(int i = 0; i < omas.Length; i++)
+            //{
+            //    //Console.Write($"omas[{i}]");
+            //    omas[i] = rnd.Next(-10, 10);
+
+            //}
+            //foreach (var element in omas)
+            //{
+            //    Console.Write($"{element}\t");
+            //}
+
+
+
+
+            //int size = 10; 
+            //int[] omas = new int[size];
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //   omas[i] = i;
+            //}
+            //Console.WriteLine("Массив:");
+            //foreach (int element in omas)
+            //{
+            //    Console.Write(element + " ");
+            //}
+            //Console.ReadKey();
+          
+        }
+    }
+}
